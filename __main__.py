690a228359bd2fa9044f31759c3329762add8052
tulare <tulare.paxgalactica@gmail.com>
2018-10-16 09:06:55 +0200

    Use pipenv to manage project dependencies

---
+++ __main__.py
@@ -0,0 +1,5 @@
+# -*- encoding: utf-8 -*-
+import calculator
+
+if __name__ == '__main__' :
+    calculator.main()
--- calculator.py
+++ calculator.py
@@ -4,7 +4,11 @@ from mvc.model import CalculatorModel
 from mvc.view import CalculatorView
 from mvc.controller import CalculatorController
 
-model = CalculatorModel()
-view = CalculatorView()
-controller = CalculatorController(model, view)
-controller.run()
+def main() :
+    model = CalculatorModel()
+    view = CalculatorView()
+    controller = CalculatorController(model, view)
+    controller.run()
+
+if __name__ == '__main__' :
+    main()
